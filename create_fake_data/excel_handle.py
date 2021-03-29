8fb9bebcacf3dca3c5572a631f183dfd29313772
toanitsv <khoailangdaknong@hotmail.com>
2021-03-29 13:21:07 +0700

    read input text

--- create_fake_data/excel_handle.py
+++ create_fake_data/excel_handle.py
@@ -7,7 +7,7 @@ import json
 def jprint(obj):
     text = json.dumps(obj, sort_keys=False, indent=4)
     print(text)
-    
+
 print('##### Reading Excel File ######')
 # duong dan cua file excel
 data_from_excel = []
@@ -25,8 +25,8 @@ try:
         cell_col_2 = sheet.cell_value(i, 1)
         cell_col_3 = sheet.cell_value(i, 2)
         # name 1 = A, name2 = B ==> tạo ra 1 cái key = AB và keySwap = BA để lọc dữ liệu cho dễ
-        row = { 'key' : '{}{}'.format(cell_col_1, cell_col_2),
-                'keySwap' : '{}{}'.format(cell_col_2, cell_col_1),            
+        row = { 'key' : '{}-{}'.format(cell_col_1, cell_col_2),
+                'keySwap' : '{}-{}'.format(cell_col_2, cell_col_1),            
                 'name1':'{}'.format(cell_col_1), 
                 'name2':'{}'.format(cell_col_2),
                 'qty':cell_col_3}
@@ -34,10 +34,65 @@ try:
 except:
     print('##### Cant Read Excel File ######')
 
+#print('##### Data from excel ######')
+#print(data_from_excel)
+
+# Reading txt file
+raw_txt = []
+try:
+    file_open = open("txt_input/" + "Input.txt")
+    file_content = file_open.readlines()
+    for line in file_content:
+        raw_txt.append(line.strip())
+except:
+    print('##### Cant read txt ######')
+
+# hàm lấy name1 hoặc name2 từ 1 dòng trong file txt
+def read_content(item):
+    # tách content giữa dấu : ( ví dụ path:C111 tách thành path và C111 )
+    if ":" in item:
+        item_split = item.split(":")
+        return item_split[1].strip()
+    else:
+        return "####"
+    
+
+# Lọc data từ file txt
+index = 0
+data_from_txt = []
+for item in raw_txt:
+    # đọc theo dòng. nếu dòng đó = item 1 thì vào lấy data.
+    if item.strip().upper() == "ITEM 1":
+        next_item1 = raw_txt[index + 1].strip().replace("\t","")
+        name1 = read_content(next_item1)
+        
+        # tìm name2
+        index_name2 = index + 1
+        while raw_txt[index_name2].strip().upper() != "ITEM 2":
+            index_name2 += 1
+        next_item2 = raw_txt[index_name2 + 1].strip().replace("\t","")
+        name2 = read_content(next_item2)
+        # tạo 1 dòng mới
+        row = { 'key' : '{}!{}'.format(name1, name2),
+                'keySwap' : '{}!{}'.format(name2, name1),
+                'name1':'{}'.format(name1), 
+                'name2':'{}'.format(name2),
+                'qty': 1}
+        data_from_txt.append(row)
+    index = index + 1
+
+print('##### Data from txt ######')
+jprint(data_from_txt)
+
+data_from_excel = data_from_txt
 # list key
 keys_unique = set(item['key'] for item in data_from_excel)
+
 # list swap key
 keys_swap_unique = set(item['keySwap'] for item in data_from_excel)
+
+print(keys_unique)
+print(keys_swap_unique)
 
 print('##### Select data ######')
 #đếm data
@@ -46,12 +101,17 @@ for key in keys_unique:
     x = key
     # nếu cái key đó có trong keySwap list
     # cập nhật lại cái key có keyswap = key ( ví dụ AB == > cập nhật thành BA) cái này là swap key[::-1] AB[::-1] = BA
+    # cập nhật dùng split để swap thay cho đảo ký tự [::-1] ( ví dụ ACBE!QET swap thành QET!ACBE )
+    keys_split = key.split("!")
+    key_swap_temp = keys_split[1] + "!" + keys_split[0]
     if key in keys_swap_unique:
-        keys_swap_unique.remove(key[::-1])
-        x = key[::-1]
+        keys_swap_unique.remove(key_swap_temp)
+        x = key_swap_temp
     # list data với điều kiện key = key hoặc key = keySwap ( ví dụ AB hoặc BA sẽ đếm là 2 )
     data_list_by_key = list(filter(lambda x: (x['key'] == key or x['keySwap'] == key), data_from_excel))
     r = {'key' : '{}'.format(x),
+         'name1' : '{}'.format(data_list_by_key[0]['name1']),
+         'name2' : '{}'.format(data_list_by_key[0]['name2']),
         'qtyMatch' : '{}'.format(len(data_list_by_key))}
     data_list.append(r)
     
@@ -78,12 +138,17 @@ def export_excel (data):
         sh = book.add_sheet('sheet 1')
         
         sh.write(0, 0, 'Clash')
-        sh.write(0, 1, 'Qty Match')
+        sh.write(0, 1, 'Name1')
+        sh.write(0, 2, 'Name2')
+        sh.write(0, 3, 'Qty Match')
         
         i = 1
         for r in data:
-            sh.write(i, 0, r['key'])
-            sh.write(i, 1, r['qtyMatch'])
+            #sh.write(i, 0, r['key'])
+            sh.write(i, 0, r['name1'] + " -> " + r['name2'])
+            sh.write(i, 1, r['name1'])
+            sh.write(i, 2, r['name2'])
+            sh.write(i, 3, r['qtyMatch'])
             i = i + 1
         book.save('excel_output/' + 'result.xls')
         return True
